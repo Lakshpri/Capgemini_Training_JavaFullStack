47343d27a0f5486cf2ffee82a4952789d9d243a0
Lakshmi Priya <lakshpri@gmail.com>
2026-01-25 10:55:11 +0530

    dsa

---
+++ Capgemini_javafullstack/src/DSA_STRINGS/BASICS.java
@@ -0,0 +1,13 @@
+package DSA_STRINGS;
+
+public class BASICS {
+   public static void main (String args[]) {
+	   String s="Lux priya 450 ERROR";
+	   System.out.println(s.length());
+	   System.out.println(s.charAt(0));
+	   System.out.println(s.contains("ERROR"));
+	   System.out.println(s.split(" "));
+	   System.out.println(s.substring(0,3));
+	   System.out.println(Integer.parseInt("450"));
+   }
+}
